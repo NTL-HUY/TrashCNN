692c978ba572bdaabcf5d2ab7e437a8a2922be38
nhathuyzrv <nhathuy57889@gmail.com>
2026-05-09 17:10:26 +0700

    fix backbone

--- TrashClassify/model.py
+++ TrashClassify/model.py
@@ -1,305 +1,172 @@
-# modified_resnet50.py
-
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 
-class ResidualBlock(nn.Module):
-    """
-    Residual Block used in ResNet50 architecture
-    """
+class Bottleneck(nn.Module):
+    expansion = 4  # output channels = planes * 4
 
-    def __init__(self, in_channels, filters, reduce=False, stride=2):
-        """
-        Arguments:
-        in_channels -- number of input channels
-        filters     -- list of filters [F1, F2, F3]
-        reduce      -- whether to reduce spatial size
-        stride      -- stride for downsampling
-        """
+    def __init__(self, in_channels, planes, stride=1, downsample=None):
+        super(Bottleneck, self).__init__()
 
-        super(ResidualBlock, self).__init__()
+        # conv1: 1x1, giảm channel
+        self.conv1 = nn.Conv2d(in_channels, planes, kernel_size=1, bias=False)
+        self.bn1   = nn.BatchNorm2d(planes)
 
-        F1, F2, F3 = filters
+        # conv2: 3x3, xử lý spatial
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
+                               stride=stride, padding=1, bias=False)
+        self.bn2   = nn.BatchNorm2d(planes)
 
-        self.reduce = reduce
+        # conv3: 1x1, tăng channel lên planes*4
+        self.conv3 = nn.Conv2d(planes, planes * self.expansion,
+                               kernel_size=1, bias=False)
+        self.bn3   = nn.BatchNorm2d(planes * self.expansion)
 
-        # Shortcut branch
-        self.shortcut = nn.Sequential()
-
-        if reduce:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(
-                    in_channels,
-                    F3,
-                    kernel_size=1,
-                    stride=stride,
-                    bias=False
-                ),
-                nn.BatchNorm2d(F3)
-            )
-
-            # Main branch first conv
-            self.conv1 = nn.Conv2d(
-                in_channels,
-                F1,
-                kernel_size=1,
-                stride=stride,
-                bias=False
-            )
-
-        else:
-            self.conv1 = nn.Conv2d(
-                in_channels,
-                F1,
-                kernel_size=1,
-                stride=1,
-                bias=False
-            )
-
-        self.bn1 = nn.BatchNorm2d(F1)
-
-        # Second conv
-        self.conv2 = nn.Conv2d(
-            F1,
-            F2,
-            kernel_size=3,
-            stride=1,
-            padding=1,
-            bias=False
-        )
-        self.bn2 = nn.BatchNorm2d(F2)
-
-        # Third conv
-        self.conv3 = nn.Conv2d(
-            F2,
-            F3,
-            kernel_size=1,
-            stride=1,
-            bias=False
-        )
-        self.bn3 = nn.BatchNorm2d(F3)
-
-        self.relu = nn.ReLU(inplace=True)
+        self.relu       = nn.ReLU(inplace=True)
+        self.downsample = downsample
 
     def forward(self, x):
+        identity = x
 
-        shortcut = x
+        out = self.relu(self.bn1(self.conv1(x)))
+        out = self.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
 
-        # Main path
-        out = self.conv1(x)
-        out = self.bn1(out)
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
         out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-        out = self.relu(out)
-
-        out = self.conv3(out)
-        out = self.bn3(out)
-
-        # Shortcut path
-        if self.reduce:
-            shortcut = self.shortcut(shortcut)
-
-        # Add
-        out += shortcut
-        out = self.relu(out)
-
         return out
 
 
 class ResNet50(nn.Module):
-
     def __init__(self, num_classes=1000):
         super(ResNet50, self).__init__()
 
-        # Initial layers
-        self.conv1 = nn.Conv2d(
-            in_channels=3,
-            out_channels=64,
-            kernel_size=7,
-            stride=2,
-            padding=3,
-            bias=False
-        )
+        self.in_channels = 64
 
-        self.bn1 = nn.BatchNorm2d(64)
-        self.relu = nn.ReLU(inplace=True)
+        self.conv1   = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1     = nn.BatchNorm2d(64)
+        self.relu    = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
-        self.maxpool = nn.MaxPool2d(
-            kernel_size=3,
-            stride=2,
-            padding=1
-        )
+        self.layer1 = self._make_layer(planes=64,  blocks=3, stride=1)
+        self.layer2 = self._make_layer(planes=128, blocks=4, stride=2)
+        self.layer3 = self._make_layer(planes=256, blocks=6, stride=2)
+        self.layer4 = self._make_layer(planes=512, blocks=3, stride=2)
 
-        # Stage 2
-        self.layer2 = nn.Sequential(
-            ResidualBlock(64, [64, 64, 256], reduce=True, stride=1),
-            ResidualBlock(256, [64, 64, 256]),
-            ResidualBlock(256, [64, 64, 256]),
-        )
-
-        # Stage 3
-        self.layer3 = nn.Sequential(
-            ResidualBlock(256, [128, 128, 512], reduce=True, stride=2),
-            ResidualBlock(512, [128, 128, 512]),
-            ResidualBlock(512, [128, 128, 512]),
-            ResidualBlock(512, [128, 128, 512]),
-        )
-
-        # Stage 4
-        self.layer4 = nn.Sequential(
-            ResidualBlock(512, [256, 256, 1024], reduce=True, stride=2),
-            ResidualBlock(1024, [256, 256, 1024]),
-            ResidualBlock(1024, [256, 256, 1024]),
-            ResidualBlock(1024, [256, 256, 1024]),
-            ResidualBlock(1024, [256, 256, 1024]),
-            ResidualBlock(1024, [256, 256, 1024]),
-        )
-
-        # Stage 5
-        self.layer5 = nn.Sequential(
-            ResidualBlock(1024, [512, 512, 2048], reduce=True, stride=2),
-            ResidualBlock(2048, [512, 512, 2048]),
-            ResidualBlock(2048, [512, 512, 2048]),
-        )
-
-        # Global Average Pooling
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc      = nn.Linear(512 * Bottleneck.expansion, num_classes)
 
-        # Fully Connected
-        self.fc = nn.Linear(2048, num_classes)
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def _make_layer(self, planes, blocks, stride=1):
+        downsample = None
+
+        # Cần downsample khi stride>1 hoặc channel thay đổi
+        if stride != 1 or self.in_channels != planes * Bottleneck.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.in_channels, planes * Bottleneck.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * Bottleneck.expansion),
+            )
+
+        layers = [Bottleneck(self.in_channels, planes, stride, downsample)]
+        self.in_channels = planes * Bottleneck.expansion
+
+        for _ in range(1, blocks):
+            layers.append(Bottleneck(self.in_channels, planes))
+
+        return nn.Sequential(*layers)
 
     def forward(self, x):
-
-        # Initial layers
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-
-        # Residual stages
+        x = self.maxpool(self.relu(self.bn1(self.conv1(x))))
+        x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
         x = self.layer4(x)
-        x = self.layer5(x)
-
-        # Pooling
         x = self.avgpool(x)
-
-        # Flatten
         x = torch.flatten(x, 1)
-
-        # Classification
         x = self.fc(x)
-
         return x
 
 
 class ModifiedResNet50(nn.Module):
-
     def __init__(self, num_classes=1000, dropout_rate=0.5):
         super(ModifiedResNet50, self).__init__()
 
-        # Initial layers
-        self.conv1 = nn.Conv2d(
-            in_channels=3,
-            out_channels=64,
-            kernel_size=7,
-            stride=2,
-            padding=3,
-            bias=False
-        )
+        self.in_channels = 64
 
-        self.bn1 = nn.BatchNorm2d(64)
-        self.relu = nn.ReLU(inplace=True)
+        # Stem
+        self.conv1   = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1     = nn.BatchNorm2d(64)
+        self.relu    = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
-        self.maxpool = nn.MaxPool2d(
-            kernel_size=3,
-            stride=2,
-            padding=1
-        )
+        # Stages
+        self.layer1 = self._make_layer(planes=64,  blocks=3, stride=1)
+        self.layer2 = self._make_layer(planes=128, blocks=4, stride=2)
+        self.layer3 = self._make_layer(planes=256, blocks=6, stride=2)
+        self.layer4 = self._make_layer(planes=512, blocks=3, stride=2)
 
-        # Stage 2
-        self.layer2 = nn.Sequential(
-            ResidualBlock(64, [64, 64, 256], reduce=True, stride=1),
-            ResidualBlock(256, [64, 64, 256]),
-            ResidualBlock(256, [64, 64, 256]),
-        )
-
-        # Stage 3
-        self.layer3 = nn.Sequential(
-            ResidualBlock(256, [128, 128, 512], reduce=True, stride=2),
-            ResidualBlock(512, [128, 128, 512]),
-            ResidualBlock(512, [128, 128, 512]),
-            ResidualBlock(512, [128, 128, 512]),
-        )
-
-        # Stage 4
-        self.layer4 = nn.Sequential(
-            ResidualBlock(512, [256, 256, 1024], reduce=True, stride=2),
-            ResidualBlock(1024, [256, 256, 1024]),
-            ResidualBlock(1024, [256, 256, 1024]),
-            ResidualBlock(1024, [256, 256, 1024]),
-            ResidualBlock(1024, [256, 256, 1024]),
-            ResidualBlock(1024, [256, 256, 1024]),
-        )
-
-        # Stage 5
-        self.layer5 = nn.Sequential(
-            ResidualBlock(1024, [512, 512, 2048], reduce=True, stride=2),
-            ResidualBlock(2048, [512, 512, 2048]),
-            ResidualBlock(2048, [512, 512, 2048]),
-        )
-
-        # Global Average Pooling
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-
-        # Dropout
         self.dropout = nn.Dropout(dropout_rate)
+        self.fc      = nn.Linear(512 * Bottleneck.expansion, num_classes)
 
-        # Fully Connected
-        self.fc = nn.Linear(2048, num_classes)
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def _make_layer(self, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.in_channels != planes * Bottleneck.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.in_channels, planes * Bottleneck.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * Bottleneck.expansion),
+            )
+
+        layers = [Bottleneck(self.in_channels, planes, stride, downsample)]
+        self.in_channels = planes * Bottleneck.expansion
+
+        for _ in range(1, blocks):
+            layers.append(Bottleneck(self.in_channels, planes))
+
+        return nn.Sequential(*layers)
 
     def forward(self, x):
-
-        # Initial layers
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-
-        # Residual stages
+        x = self.maxpool(self.relu(self.bn1(self.conv1(x))))
+        x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
         x = self.layer4(x)
-        x = self.layer5(x)
-
-        # Pooling
         x = self.avgpool(x)
-
-        # Flatten
         x = torch.flatten(x, 1)
-
-        # Dropout
         x = self.dropout(x)
-
-        # Classification
         x = self.fc(x)
-
         return x
 
 
-# Testing
 if __name__ == "__main__":
-
-    model = ModifiedResNet50(num_classes=10)
-
+    model = ModifiedResNet50(num_classes=7)
     x = torch.randn(4, 3, 224, 224)
-
     y = model(x)
-
     print(model)
     print("Output shape:", y.shape)
+
+    # Kiểm tra key có khớp torchvision không
+    from torchvision.models.detection.backbone_utils import resnet_fpn_backbone
+    backbone = resnet_fpn_backbone('resnet50', pretrained=False)
+    classify_keys = set(model.state_dict().keys())
+    backbone_keys = set(backbone.body.state_dict().keys())
+    shared = classify_keys & backbone_keys
+    print(f"\nKeys khớp với FPN backbone: {len(shared)} / {len(backbone_keys)}")
--- TrashDetect/model.py
+++ TrashDetect/model.py
@@ -3,13 +3,45 @@ from torchvision.models.detection import FasterRCNN
 from torchvision.models.detection.backbone_utils import resnet_fpn_backbone
 
 
+def load_backbone_weights(backbone, weights_path):
+    print(f"--- Loading backbone weights from: {weights_path} ---")
+    checkpoint = torch.load(weights_path, map_location="cpu")
+
+    if isinstance(checkpoint, dict) and "model" in checkpoint:
+        state_dict = checkpoint["model"]
+        print("  [OK] Extracted 'model' key from checkpoint dict.")
+    else:
+        state_dict = checkpoint
+        print("  [OK] Checkpoint is a raw state_dict.")
+
+    backbone_state = backbone.body.state_dict()
+    matched = {
+        k: v for k, v in state_dict.items()
+        if k in backbone_state and backbone_state[k].shape == v.shape
+    }
+    missing    = [k for k in backbone_state if k not in matched]
+    unexpected = [k for k in state_dict if k not in backbone_state]
+
+    backbone.body.load_state_dict(matched, strict=False)
+
+    total = len(backbone_state)
+    print(f"  [OK] Matched layers  : {len(matched)} / {total}")
+    print(f"  [!!] Missing layers  : {len(missing)}")
+    print(f"  [!!] Unexpected keys : {len(unexpected)}")
+
+    if len(matched) == 0:
+        print("\n  [ERROR] Không có layer nào được load!")
+    elif len(matched) < total * 0.8:
+        print(f"\n  [WARNING] Chỉ load được {len(matched)}/{total} layers.")
+    else:
+        print(f"  [OK] Backbone weights loaded successfully.")
+
+    return backbone
+
+
 def freeze_backbone_selective(backbone):
     for param in backbone.parameters():
         param.requires_grad = False
-
-    for name, param in backbone.body.named_parameters():
-        if name.startswith("layer4."):
-            param.requires_grad = True
 
     for param in backbone.fpn.parameters():
         param.requires_grad = True
@@ -21,10 +53,9 @@ def freeze_backbone_selective(backbone):
     return backbone
 
 
-def build_model(num_classes, my_weights_path=None):
-    backbone = resnet_fpn_backbone('resnet50', pretrained=True)
-
+def build_model(num_classes, my_weights_path):
+    backbone = resnet_fpn_backbone('resnet50', pretrained=False)
+    backbone = load_backbone_weights(backbone, my_weights_path)
     backbone = freeze_backbone_selective(backbone)
-
     model = FasterRCNN(backbone, num_classes=num_classes)
     return model
